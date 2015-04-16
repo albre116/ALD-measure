f630301fe24103d14f8d24a5a178f1f50b614ce7
Nick Strayer <nstrayer@uvm.edu>
2015-04-16 18:24:58 -0400

    Added the multiple color scales with a quick little sequence function to help

--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -14,12 +14,21 @@ if (!is.installed("fields")){
 
 script <- "
 var homzScale = d3.scale.linear()
-  .domain([0, 1])
-  .range(['#4daf4a', '#e41a1c'])
+    .domain( [0,         .33,        .66,        1])
+    .range(['#66bd63', '#fee08b', '#f46d43', '#d73027'])
+
 
 var freqScale = d3.scale.linear()
-  .domain([0, .3])
-  .range(['#4daf4a', '#e41a1c'])
+.domain( [0,         .33,        .66,        1])
+.range(['#66bd63', '#fee08b', '#f46d43', '#d73027'])
+
+
+var domainGen = function(ar){
+var start = ar[0]
+var end = ar[1]
+var range = end - start
+return [start, start + (0.33*range), start + (0.66*range), end]
+}
 
 //Waits to execute the javascript code 200 miliseconds. This is needed because shiny loads the javascript before it loads the table
 //and thus has nothing to color.
@@ -28,26 +37,26 @@ freqTableVals = [] // initialize array to store table values for color scaling.
 homzTableVals = []
 
 window.setInterval(function() {
-  freqTableVals = []
-  homzTableVals = []
-  //Grab the allele freq column's max and mins
-  d3.selectAll('#asf_table tbody tr td:nth-child(4)')
-    .each(function() {
-      var cellValue = d3.select(this).text();
-      freqTableVals.push(parseFloat(cellValue))
-    })
-  
-  freqColorRange = d3.extent(freqTableVals)
-  freqScale.domain(freqColorRange)
-  
-  //grab the homz column's max and mins
-  d3.selectAll('#asf_table tbody tr td:nth-child(5)')
-    .each(function() {
-      var cellValue = d3.select(this).text();
-      homzTableVals.push(parseFloat(cellValue))
-    })
-  homzColorRange = d3.extent(homzTableVals)
-  homzScale.domain(homzColorRange)
+freqTableVals = []
+homzTableVals = []
+//Grab the allele freq column's max and mins
+  d3.selectAll('#DataTables_Table_0 tbody tr td:nth-child(4)')
+.each(function() {
+var cellValue = d3.select(this).text();
+freqTableVals.push(parseFloat(cellValue))
+})
+
+freqColorRange = d3.extent(freqTableVals)
+freqScale.domain(domainGen(freqColorRange))
+
+//grab the homz column's max and mins
+  d3.selectAll('#DataTables_Table_0 tbody tr td:nth-child(5)')
+.each(function() {
+var cellValue = d3.select(this).text();
+homzTableVals.push(parseFloat(cellValue))
+})
+homzColorRange = d3.extent(homzTableVals)
+homzScale.domain(domainGen(homzColorRange))
   
   //Set the colors for the allele freq
   d3.selectAll('#asf_table tbody tr td:nth-child(4)')
