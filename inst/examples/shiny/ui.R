065656de8d332fdc15d34d2e9c8d7b42de9335c4
rsingle <rsingle@uvm.edu>
2015-04-22 12:20:15 -0400

    added file format link and minor changes to left panel of ui.R

--- inst/examples/shiny/ui.R
+++ inst/examples/shiny/ui.R
@@ -4,23 +4,18 @@ shinyUI(fluidPage(
   titlePanel("ALD: Asymmetric Linkage Disequilibrium"),
   sidebarLayout(
     sidebarPanel(
-      fileInput('file1', 'Choose Frequency File',
-        accept=c('text/csv', 
-          'text/comma-separated-values,text/plain', 
-          '.csv')),
-      
-      tags$p("Need to see file formats?"),
-      tags$a(href="shiny.rstudio.com/tutorial", "Click Here!"),
-      tags$hr(),
-      
+      tags$a(href="http://www.uvm.edu/~rsingle/software/shiny_ALD/file_formats.txt", target="_blank", "Click here to see the file formats"),
+      fileInput('file1', 'Upload a Haplotype Frequency File',
+        accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
      #checkboxInput('header', 'Header', TRUE),      
       radioButtons('sep', 'Separator', c(Comma=',', Semicolon=';', Tab='\t'), ','),
       numericInput('tol', label = "tolerance (for sum of haplo.freqs)", value = 0.01, min = 0.01, max = 0.1, step = 0.01),
+      tags$p("(sum of haplo.freqs + tolerance) must be >= 1"),
       tags$hr(),
-      paste("ALD Plot options"),
+      tags$h4("ALD Plot options"),
       checkboxInput('values', 'Show ALD values', TRUE),
       tags$hr(),
-      paste("Allele Specific Homozygosity options"),
+      tags$h4("Allele Specific Homozygosity options"),
       uiOutput('choose_locus_pair'), #input$ var: selected_pair
       uiOutput('choose_locus')       #input$ var: selected_locus
     ),
