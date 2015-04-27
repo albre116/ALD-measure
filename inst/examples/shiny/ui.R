5df5aee3fc784edc4dd4782a1f7e6c77ad952cf7
rsingle <rsingle@uvm.edu>
2015-04-27 16:51:09 -0400

    first pass at using navbar to have context dependent left panels

--- inst/examples/shiny/ui.R
+++ inst/examples/shiny/ui.R
@@ -1,40 +1,58 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  titlePanel("ALD: Asymmetric Linkage Disequilibrium"),
-  sidebarLayout(
-    sidebarPanel(
-      tags$a(href="http://www.uvm.edu/~rsingle/software/shiny_ALD/file_formats.txt", target="_blank", "Click here to see the file formats"),
-      fileInput('file1', 'Upload a Haplotype Frequency File',
-        accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
-     #checkboxInput('header', 'Header', TRUE),      
-      radioButtons('sep', 'Separator', c(Comma=',', Semicolon=';', Tab='\t'), ','),
-      numericInput('tol', label = "tolerance (for sum of haplo.freqs)", value = 0.01, min = 0.01, max = 0.1, step = 0.01),
-      tags$p("(sum of haplo.freqs + tolerance) must be >= 1"),
-      tags$hr(),
-      tags$h4("ALD Plot options"),
-      checkboxInput('values', 'Show ALD values', TRUE),
-      tags$hr(),
-      tags$h4("Allele Specific Homozygosity options"),
-      uiOutput('choose_locus_pair'), #input$ var: selected_pair
-      uiOutput('choose_locus')       #input$ var: selected_locus
-    ),
+shinyUI(navbarPage("Asymmetric LD (ALD)",
+                   
+  tabPanel("Upload Data", 
+    sidebarLayout(
+      sidebarPanel(
+        fileInput('file1', 'Upload a Haplotype Frequency File',
+                  accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+        #checkboxInput('header', 'Header', TRUE),      
+        tags$a(href="http://www.uvm.edu/~rsingle/software/shiny_ALD/file_formats.txt", target="_blank", "Click here to see the file formats"),
 
-    mainPanel(
-      tags$head(
-        tags$script(src="d3.js"),
-        tags$script(src="lodash.js")
-        ),
-      tabsetPanel(type = "tabs", 
-        tabPanel("ALD Plot", plotOutput('heatmap')), 
-        tabPanel("Allele Specific Homozygosity Table", uiOutput("asf_display")),
-        tabPanel("ALD Table", 
-                 htmlOutput("text_ALDdata1"),
-                 dataTableOutput('plot_data')),
-        tabPanel("raw Data", dataTableOutput('raw_data'))
+        radioButtons('sep', 'Separator', c(Comma=',', Semicolon=';', Tab='\t'), ','),
+        numericInput('tol', label = "tolerance (for sum of haplo.freqs)", value = 0.01, min = 0.01, max = 0.1, step = 0.01),
+        tags$p("(sum of haplo.freqs + tolerance) must be >= 1")
+      ),
+      mainPanel(
+        titlePanel("Asymmetric Linkage Disequilibrium (ALD)"),
+        helpText("Note: something about tolerance ..."),
+        textOutput("text_rawdata1"),
+        
+        dataTableOutput('raw_data')
       )
     )
-    
-    
+  ),
+
+  tabPanel("ALD Plot", 
+    sidebarLayout( 
+     sidebarPanel(
+       tags$h4("ALD plot options"),
+       checkboxInput('values', 'Show ALD values', TRUE)
+     ),
+     mainPanel(
+       plotOutput('heatmap')
+     )
+    )
+  ),
+  
+  tabPanel("ALD Table", 
+           htmlOutput("text_ALDdata1"),
+           dataTableOutput('plot_data')
+  ),
+  
+  tabPanel("Allele Specific Homozygosity Table", 
+    sidebarLayout( 
+      sidebarPanel(
+        tags$h4("Allele Specific Homozygosity options"),
+        uiOutput('choose_locus_pair'), #input$ var: selected_pair
+        uiOutput('choose_locus')       #input$ var: selected_locus
+      ),
+      mainPanel(
+        uiOutput("asf_display")
+      )
+    )
   )
 ))
+
+  
