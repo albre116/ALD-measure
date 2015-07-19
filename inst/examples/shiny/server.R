8035f3ebcbbf7a0de3a643cd0fe4a7d90d6869f3
Nick Strayer <nstrayer@uvm.edu>
2015-07-18 19:33:02 -0700

    No luck yet. A few print flags placed around.

--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -5,7 +5,7 @@ library(asymLD)
 # Function to check whether package is installed
 is.installed <- function(mypkg){
   is.element(mypkg, installed.packages()[,1])
-} 
+}
 
 if (!is.installed("fields")){
   install.packages("fields")
@@ -76,16 +76,16 @@ d3.selectAll('#asf_table tbody tr td:nth-child(5)')
 var cellValue = d3.select(this).text();
 return (homzScale(cellValue))
 })
-//this is the amount of miliseconds between runs of the function. 
+//this is the amount of miliseconds between runs of the function.
 }, 200);
 "
 
 shinyServer(function(input, output, session) {
-  
+
   session$onFlushed(function() {
     session$sendCustomMessage(type='jsCode', list(value = script))
   }, once = FALSE)
-  
+
   dataInput <- reactive({
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, it will be a data frame with 'name',
@@ -101,9 +101,9 @@ shinyServer(function(input, output, session) {
       return(NULL)
     } else {
       output$text_fileinput <- renderText({ paste("") })
-      # remove haplotypes with freq == 0 
+      # remove haplotypes with freq == 0
       rm.inds <- data[, dim(data)[2]] == 0
-      data <- data[!rm.inds, ]  
+      data <- data[!rm.inds, ]
       return(data)
     }
   })
@@ -112,7 +112,7 @@ shinyServer(function(input, output, session) {
     data <- dataInput()
     data.type2 <- TRUE
     names.type2 <- c("locus1", "locus2", "allele1", "allele2", "haplo.freq")
-    check.names <- names.type2 %in% names(data)    
+    check.names <- names.type2 %in% names(data)
     if (sum(!check.names) > 0) data.type2 <- FALSE
     if (is.null(data))
       return(NULL)
@@ -129,8 +129,8 @@ shinyServer(function(input, output, session) {
       else {
         output$text_rawdata1 <- renderText({ paste("Sum of haplo.freqs (min over locus pairs) = ", min.hf.sum) })
         output$text_rawdata2 <- renderText({ paste("") })
-        return(data)        
-      }       
+        return(data)
+      }
     } else {
       sum.freqs <- sum(data[, dim(data)[2]])
       if ( sum.freqs < 1 - input$tol) {
@@ -141,28 +141,28 @@ shinyServer(function(input, output, session) {
       else {
         output$text_rawdata1 <- renderText({ paste("Sum of haplo.freqs = ", sum.freqs) })
         output$text_rawdata2 <- renderText({ paste("") })
-        return(data)        
+        return(data)
       }
-    } 
-  }  
-  })  
-  
+    }
+  }
+  })
+
   # display data
   output$raw_data <- renderDataTable({
     dataInput2()
   })
-   
+
   # prepare the data for plotting function
   plotData <- reactive({
-    withProgress(message = "Computing ALD values...", value = 0, {  
-    #Sys.sleep(2) #useful for debugging progress bar stuff. 
+    withProgress(message = "Computing ALD values...", value = 0, {
+    #Sys.sleep(2) #useful for debugging progress bar stuff.
     data <- dataInput2()
     if (is.null(data))
       return(NULL)
     else {
       data.type2 <- TRUE
       names.type2 <- c("locus1", "locus2", "allele1", "allele2", "haplo.freq")
-      check.names <- names.type2 %in% names(data)    
+      check.names <- names.type2 %in% names(data)
       if (sum(!check.names) > 0) data.type2 <- FALSE
       if (data.type2){
         data$locus <- paste(data$locus1, data$locus2, sep="-")
@@ -171,7 +171,7 @@ shinyServer(function(input, output, session) {
         out<- by(data, list(locus=data$locus), compute.ALD)
         rbind.out <- NULL
         for (i in 1:length(out)) rbind.out <- rbind(rbind.out,out[[i]])
-        ald.allpairs <- rbind.out 
+        ald.allpairs <- rbind.out
       } else {
        #pop.use <- names(data)[dim(data)[2]]
         nloci <- dim(data)[2]-1
@@ -180,15 +180,15 @@ shinyServer(function(input, output, session) {
         for (i in 1:(nloci-1)){
           for (j in (i+1):nloci){
             bi.data <- get_bilocus_data(data, i, j)
-            ald.allpairs <- rbind(ald.allpairs, compute.ALD(bi.data, tolerance=input$tol))      
+            ald.allpairs <- rbind(ald.allpairs, compute.ALD(bi.data, tolerance=input$tol))
           }
-        }        
+        }
       }
       ald.allpairs
     }
     })
   })
-  
+
   # display statistic definitions above plotData
   output$text_ALDdata1 <- renderText({ paste(pre(
     "F.1     Homozygosity (expected under HWP) for locus 1",
@@ -197,9 +197,9 @@ shinyServer(function(input, output, session) {
     "F.2.1   Conditional homozygosity for locus2 given locus1",
     "ALD.1.2 Asymmetric LD for locus1 given locus2",
     "ALD.2.1 Asymmetric LD for locus2 given locus1"
-    )) 
+    ))
   })
-  
+
   # display plotData
   output$plot_data <- renderDataTable({
     plotData()
@@ -209,8 +209,8 @@ shinyServer(function(input, output, session) {
     data <- plotData()
     data$locus <- paste(data$locus1, data$locus2, sep="-")
     loci <- data$locus
-    selectInput("selected_pair", "Choose a locus pair:", as.list(loci)) 
-  })  
+    selectInput("selected_pair", "Choose a locus pair:", as.list(loci))
+  })
   output$choose_locus <- renderUI({
     if (is.null(plotData()))
       return(NULL)
@@ -223,9 +223,9 @@ shinyServer(function(input, output, session) {
         #nothing
       }, finally = {
         #all good
-      }                
+      }
                         )
-      selectInput("selected_locus", "Choose the focal locus:", as.list(loci)) 
+      selectInput("selected_locus", "Choose the focal locus:", as.list(loci))
     }
   })
 
@@ -235,7 +235,7 @@ shinyServer(function(input, output, session) {
     else {
       data <- plotData()
       loci <- unique(c(as.character(data$locus1),as.character(data$locus2)))
-      selectInput("selected_locus1", "Subset: Choose first locus", as.list(loci[-length(loci)])) 
+      selectInput("selected_locus1", "Subset: Choose first locus", as.list(loci[-length(loci)]))
     }
   })
   output$choose_locus2 <- renderUI({
@@ -248,11 +248,11 @@ shinyServer(function(input, output, session) {
       if (length(loci) <= 25) locus2.no.default <- length(loci)
       else locus2.no.default <- locus1.no + min(25-1, length(loci)-locus1.no)
       loci.subset <- loci[(locus1.no + 1):locus2.no.default] #loci.subset <- loci[(locus1.no + 1):length(loci)]
-      selectInput("selected_locus2", "Subset: Choose last locus", as.list(loci.subset), selected=loci[locus2.no.default]) 
+      selectInput("selected_locus2", "Subset: Choose last locus", as.list(loci.subset), selected=loci[locus2.no.default])
     }
-  })  
-  
-  # plot of asymetric LD      
+  })
+
+  # plot of asymetric LD
   output$heatmap <- renderPlot({
     withProgress(message = "Rendering Plot...", value = 0, {
     data <- plotData()
@@ -263,23 +263,23 @@ shinyServer(function(input, output, session) {
       if (is.null(input$selected_locus2)) loci.no <- 1:2
       else loci.no <- (1:length(loci))[loci %in% c(input$selected_locus1,input$selected_locus2)]
       loci.subset <- loci[(loci.no[1]):(loci.no[2])]
-      
+
       incr.outer.marg <- par( mar=c(3.4,5.5,5.6,1.8)+.1 ) #c(bottom, left, top, right) default: c(5, 4, 4, 2) + 0.1
-      ld.matrix.plot.2vars(dat=data, 
+      ld.matrix.plot.2vars(dat=data,
                            ld.varnames=c("ALD.1.2","ALD.2.1"), map.order=loci.subset,
-                           ld.labnames=c("",""), bw=T, xlab.shift=1, ylab.shift=-0, 
+                           ld.labnames=c("",""), bw=T, xlab.shift=1, ylab.shift=-0,
                            values=input$values)
-      #title(sub=paste("Asymmetric Linkage Disequilibrium\n row gene conditional on column gene"),font.sub=2,cex.sub=1.2)            
+      #title(sub=paste("Asymmetric Linkage Disequilibrium\n row gene conditional on column gene"),font.sub=2,cex.sub=1.2)
       text.sub <- paste("Asymmetric Linkage Disequilibrium\n row gene conditional on column gene")
       mtext(text.sub, side = 1, padj=1, font=2, cex=1.2)
       par(incr.outer.marg)
     }
     })
   })
-  
+
 ########################################################################################################################
 # asf table
-#Calculate the table inside of a reactive function. 
+#Calculate the table inside of a reactive function.
   calcTable <- reactive({
     data <- dataInput2()
     if (is.null(data))
@@ -287,7 +287,7 @@ shinyServer(function(input, output, session) {
     else {
       data.type2 <- TRUE
       names.type2 <- c("locus1", "locus2", "allele1", "allele2", "haplo.freq")
-      check.names <- names.type2 %in% names(data)    
+      check.names <- names.type2 %in% names(data)
       if (sum(!check.names) > 0) data.type2 <- FALSE
       if (data.type2){
         #loci <- c( unique(as.character(data$locus1)), unique(as.character(data$locus2)) )
@@ -305,15 +305,16 @@ shinyServer(function(input, output, session) {
         bi.data$locus2 <- as.character(bi.data$locus2)
         table <- compute.AShomz(bi.data, sort.var=c("focal","allele.freq"), sort.asc=c(F,F), tolerance=input$tol)
       }
-      table[table$focal==input$selected_locus,]        
+      print("I HAVE RUN!")
+      table[table$focal==input$selected_locus,]
     }
   })
-  
+
   output$asf_table <- renderDataTable({
     #Run the table function inside of the renderDataTable
-    #Wrapped it in a try catch because it was spilling out an error about undefined columns before finishing. 
+    #Wrapped it in a try catch because it was spilling out an error about undefined columns before finishing.
     tryCatch({
-      calcTable() 
+      calcTable()
     }, warning = function(w) {
       #nothing
     }, error = function(e) {
@@ -323,18 +324,21 @@ shinyServer(function(input, output, session) {
     })
   }, options = list(orderClasses = TRUE))
   #Write the max and min values to the html, these will later be made hidden with css, They are for the javascript to use for
-  #calculating the max and min for color scales. 
-  
+  #calculating the max and min for color scales.
+
 getMax = function(loc){
+    toReturn = 1
   tryCatch({
-    return(max(loc))
+    toReturn = max(loc)
+    print("getMax() ran without an error!")
   }, warning = function(w) {
-    #nothing
+
   }, error = function(e) {
-    #nothing
+
   }, finally = {
     #all good
   })
+  return(toReturn)
 }
 
 getMin = function(loc){
@@ -348,14 +352,16 @@ getMin = function(loc){
     #all good
   })
 }
-#  output$maxVal_f <- renderText({ paste(getMax(calcTable()$allele.freq))})
-#  output$minVal_f <- renderText({ paste(getMin(calcTable()$allele.freq))})
-#  output$maxVal_h <- renderText({ paste(getMax(calcTable()$as.homz))})
-#  output$minVal_h <- renderText({ paste(getMin(calcTable()$as.homz))})
+ # output$maxVal_f <- renderText({ paste(getMax(calcTable()$allele.freq))})
+ # output$minVal_f <- renderText({ paste(getMin(calcTable()$allele.freq))})
+ # output$maxVal_h <- renderText({ paste(getMax(calcTable()$as.homz))})
+ # output$minVal_h <- renderText({ paste(getMin(calcTable()$as.homz))})
 #NB: above commented since AShomz coloring did not work w/ A~C~B~DRB1~DQB1_BroadRace-CAU.csv
 
-maxFreq <<- 0
-output$maxVal_f <- renderText({ paste(max(calcTable()$allele.freq))})
+# maxFreq <- getMax(calcTable()$allele.freq)
+# print("THE MAX FREQ IS!")
+# print(maxFreq)
+output$maxVal_f <- renderText({ paste(getMax(calcTable()$allele.freq))})
 output$minVal_f <- renderText({ paste(0) }) #renderText({ paste(min(calcTable()$allele.freq))})
 output$maxVal_h <- renderText({ paste(1) }) #renderText({ paste(max(calcTable()$as.homz))})
 output$minVal_h <- renderText({ paste(0) }) #renderText({ paste(min(calcTable()$as.homz))})
@@ -369,13 +375,13 @@ output$minVal_h <- renderText({ paste(0) }) #renderText({ paste(min(calcTable()$
       , dataTableOutput("asf_table")
     )
   })
-  
+
 ########################################################################################################################
-  
+
   # -------------------------- HELPER FUNCTIONS -------------------------------
-  
+
   # ---------------------------------------------------------------------------
-  # Get the frequency data for two loci from the frequency file and format it 
+  # Get the frequency data for two loci from the frequency file and format it
   # for the compute.ALD() function.
   # ---------------------------------------------------------------------------
   get_bilocus_data <- function(data, i, j){
@@ -388,90 +394,90 @@ output$minVal_h <- renderText({ paste(0) }) #renderText({ paste(min(calcTable()$
     haplo.freq <- as.numeric(aggregate.freqs)
     alleles <- t(data.frame(strsplit(allele_combos, split='-')))
     colnames(alleles) <- c('allele1', 'allele2')
-    bi.data <- cbind(data.frame(haplo.freq, locus1=rep(loci[i], length(haplo.freq)), 
+    bi.data <- cbind(data.frame(haplo.freq, locus1=rep(loci[i], length(haplo.freq)),
       locus2=rep(loci[j], length(haplo.freq))), alleles)
     row.names(bi.data) <- NULL
     return(bi.data)
   }
-  
+
   # ---------------------------------------------------------------------------
   # Function for plotting the ALD heatmap
   # ---------------------------------------------------------------------------
-  
-  ld.matrix.plot.2vars <- function(dat, ld.varnames, map.order, 
+
+  ld.matrix.plot.2vars <- function(dat, ld.varnames, map.order,
     ld.labnames=ld.varnames, bw=FALSE, xlab.shift=1, ylab.shift=-5, values=FALSE, cex.ld=.9) {
     dat2 <- dat
-    
-    # swap order of locus1 & locus2 in cases where they are not listed in 
+
+    # swap order of locus1 & locus2 in cases where they are not listed in
     # map.order with locus1 coming before locus2
     dat2 <- dat2[dat2$locus1 %in% map.order & dat2$locus2 %in% map.order,]
     dat2$locus1 <- factor(dat2$locus1, levels=map.order)
     dat2$locus2 <- factor(dat2$locus2, levels=map.order)
-    dat2$swap <- rep(0,dim(dat2)[1]) 
+    dat2$swap <- rep(0,dim(dat2)[1])
     dat2$swap[as.numeric(dat2$locus1) > as.numeric(dat2$locus2)] <- 1
     dat2$swap.locus1 <- dat2$locus1
     dat2$locus1[dat2$swap==1] <- dat2$locus2[dat2$swap==1]
-    dat2$locus2[dat2$swap==1] <- dat2$swap.locus1[dat2$swap==1]  
-    
+    dat2$locus2[dat2$swap==1] <- dat2$swap.locus1[dat2$swap==1]
+
     # make local copies of the vars on the dataframe to use from now on
     pval.color1 <- dat2[,names(dat2)==ld.varnames[1]]
     pval.color2 <- dat2[,names(dat2)==ld.varnames[2]]
     locus1 <- dat2$locus1
     locus2 <- dat2$locus2
-    
+
     n.loci <- length(map.order)
     loci <- 1:length(map.order)
     names(loci) <- map.order
-    
+
     x <- 1:n.loci
     y <- 1:n.loci
     names(x) <- map.order
     names(y) <- map.order
-    
+
     # set up a matrix w/ rownames & colnames = loci.ordered
-    z <- outer(x, y, FUN="+") 
-    
-    for (i in 1:(n.loci-1)) 
+    z <- outer(x, y, FUN="+")
+
+    for (i in 1:(n.loci-1))
     {
       for (j in i:(n.loci))
       {
-        if (i == j) 
+        if (i == j)
         {
           z[i,j] <- -1 # a value outside of the zlim range for the diagonal
-        } else 
+        } else
         {
           # ld.varnames[1] on the upper triangle
-          if (sum(locus1==names(x)[i] & locus2==names(y)[j]) > 0)  
+          if (sum(locus1==names(x)[i] & locus2==names(y)[j]) > 0)
           {
             #1st entry from pval.color1
-            z[i,j] <- pval.color1[locus1==names(x)[i] & locus2==names(y)[j]] 
+            z[i,j] <- pval.color1[locus1==names(x)[i] & locus2==names(y)[j]]
           } else
           {
             z[i,j] <- -1
           }
           # ld.varnames[2] on the lower triangle
-          if (sum(locus1==names(x)[i] & locus2==names(y)[j]) > 0)  
+          if (sum(locus1==names(x)[i] & locus2==names(y)[j]) > 0)
           {
             #2nd entry from pval.color2
-            z[j,i] <- pval.color2[locus1==names(x)[i] & locus2==names(y)[j]] 
+            z[j,i] <- pval.color2[locus1==names(x)[i] & locus2==names(y)[j]]
           } else
           {
             z[j,i] <- -1
           }
-        }  
+        }
       }
     }
     z[n.loci,n.loci] <- -1 # take care of bottom right element
-    
+
     zz <- NULL
-    for (i in (n.loci):1) 
+    for (i in (n.loci):1)
     {
       zz <- cbind(zz, z[i,]) # rotate matrix z by +90 degrees
     }
-    
+
     # NB: can change zz, but not z, since z is the return value
     zz[zz==-1] <- 9 # code untyped locus pairs as 9 for coloring
-    
+
     # plot the LD values
     z.lim <- c(0,1) #zlim: plot only considers values between these 2 values
     stat.breaks <- c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1)
@@ -479,24 +485,24 @@ output$minVal_h <- renderText({ paste(0) }) #renderText({ paste(min(calcTable()$
     if (!bw) stat.colors <- c("gray",rev(rainbow((length(stat.breaks)-1)))[-(length(stat.breaks)-10)])
     # zlim: only use colors in this range
     fields::image.plot(x,y,zz,xaxt="n",yaxt="n",axes=F,xlab="",ylab="",zlim=z.lim,
-      horizontal=F,col=stat.colors,breaks=stat.breaks) 
-    
+      horizontal=F,col=stat.colors,breaks=stat.breaks)
+
     # Add a light grid with dashed lines
     # NB: abline() must be called before mtext() and axis()
     # abline(h=0.5:(max(y)+.5),v=0.5:(max(x)+.5),lty=2,col=gray(.3))
     abline(h=0.5:(max(y)+.5),v=0.5:(max(x)+.5),lty=1,col="black")
-    
-    
+
+
     # add 1st var name as a text string rotated 90 degrees on the right margin
-    mtext(side=4,ld.labnames[1],srt=90,font=2,line=ylab.shift) 
+    mtext(side=4,ld.labnames[1],srt=90,font=2,line=ylab.shift)
     # add 2nd var name as a text string on the bottom margin
-    mtext(side=1,ld.labnames[2], font=2,line=xlab.shift) 
-    
-    # Add axes with locus names  
+    mtext(side=1,ld.labnames[2], font=2,line=xlab.shift)
+
+    # Add axes with locus names
     axis(3, at=x, labels=names(x), tick=F,las=2)
-    axis(2, at=y, labels=rev(names(y)), tick=F,las=2) # rev() since rotated matrix 90 degrees 
-    
-    
+    axis(2, at=y, labels=rev(names(y)), tick=F,las=2) # rev() since rotated matrix 90 degrees
+
+
     if (values)
     {
       for (x in 1:n.loci)
@@ -512,10 +518,10 @@ output$minVal_h <- renderText({ paste(0) }) #renderText({ paste(min(calcTable()$
               yyz <- substr(yyz,2,4)
               if (zz[x,y]>.5 & bw==T)
                 # font=2 for bold, NB: cex was 1.2
-                text(x,y,yyz,cex=cex.ld,col="white",font=1) 
-              else 
+                text(x,y,yyz,cex=cex.ld,col="white",font=1)
+              else
                 # font=2 for bold, NB: cex was 1.2
-                text(x,y,yyz,cex=cex.ld,col="black",font=1) 
+                text(x,y,yyz,cex=cex.ld,col="black",font=1)
             }
           }
         }
@@ -523,7 +529,6 @@ output$minVal_h <- renderText({ paste(0) }) #renderText({ paste(min(calcTable()$
     }
     return(z)
   }
-  
-  
-  })
 
+
+  })
